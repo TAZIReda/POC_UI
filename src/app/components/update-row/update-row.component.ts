1a33ac1ebce5a46a10fa6bd87610c88e319ba7d3
mrtazir <mohamedreda.tazir@smartest.dz>
2024-07-10 11:04:30 +0100

    create update component

---
+++ src/app/components/update-row/update-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-row.component.html',
+  styleUrl: './update-row.component.scss'
+})
+export class UpdateRowComponent {
+
+}
